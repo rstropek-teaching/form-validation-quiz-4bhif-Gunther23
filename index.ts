95e4bb5a7c4a9f9fc2d59806d02cefafbbec1eb9
Gunther23 <guenther.julian@gmx.at>
2017-11-19 16:03:35 +0100

    Add files via upload

--- index.ts
+++ index.ts
@@ -1,35 +1,25 @@
-//funktioniert nicht, werde eine neue Version über das Wochenende hochladen! 
-
-
-var firstName = <HTMLInputElement>document.getElementById("firstName");
-var lastName = <HTMLInputElement>document.getElementById("lastName");
-var email = <HTMLInputElement>document.getElementById("email");
-var mediaChannelSelect = <HTMLInputElement>document.getElementById("mediaChannelSelect");
-
-
-function btnOnclick(){
-    var button = <HTMLInputElement>document.getElementById("sub");
-
-    button.addEventListener("submit", (e:Event) => validate());
-
-
-
-}
-
-function validate(){
-    if(firstName == null){
-        alert("First Name is mandatory"); 
-    }
-
-    if(lastName == null){
-        alert("Last Name is mandatory"); 
-    }
-
-    if(email == null){
-        alert("First Name is mandatory"); 
-    }
-
-}
+$(document).ready(function(){
+    $('#form').validate({
+        rules: {
+            firstName: {
+                required: true
+            },
+            lastName: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            newsletter: {
+                required:true  
+            }
 
 
 
+        }
+    })
+})
+
+
+ 
